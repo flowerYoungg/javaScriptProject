7f10bbd9f84d1e9a456eaec2db674727d1367ebc
flowerYoungg <khy1896@gmail.com>
2021-07-07 20:56:23 +0900

    July7

---
+++ function.js
@@ -0,0 +1,39 @@
+// Function
+// -fundamental building block in the program
+// -subprogram can be used multiple times
+// -performs a task or calculates a value
+
+// 1. Function declaration
+// function name(param1, param2) {body... return;}
+// one function === one thing
+// naming : doSomething, command, verb
+// e.g. createCardAndPoint -> createCard, createPoint
+// function is object in JS
+
+// 1.1. Function expression
+// a function declaration can be called earlier than it is defined (hoisted)
+// a function expression is created when the execution reaches it
+
+// 1.2. Callback function using function expression
+
+// 1.3. Arrow function
+// always anonymous
+// const add = (a, b) => a + b;
+
+// 1.4. IIFE : Immediately Invoked Function Expression
+
+// 2.Parameters
+// premitive parameters : passed by value
+// object parameters : passed by reference
+
+// 3.Default parameters(added in ES6)
+// undefined or unknown
+
+// 4. Rest parameters(added in ES6)
+// ...A -> array A
+
+// 5. Local scope
+// global variable, local variable
+
+// 6. Return a value
+// 7. Early return, early exit
