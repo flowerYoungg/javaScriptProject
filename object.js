752a5792b530cc0770d59cae926752f0ed65344b
flowerYoungg <khy1896@gmail.com>
2021-07-13 22:40:11 +0900

    July13

---
+++ object.js
@@ -0,0 +1,25 @@
+// Objects
+// one of the JavaScript's data types
+// a collection of related data and/or functionality
+// Nearly all objects in JavaScript are instances of Object
+// object = {key : value};
+
+// 1. Literals and properties
+// can add/delete properties later
+
+// 2. Computed properties
+// key should be always string
+// console.log(liv.['name']);
+
+// 3. Property value shorthand
+
+// 4. Constructor Function
+
+// 5. in operator : property existance check(key in object)
+
+// 6. for..in vs for..of
+// for (key in obj)
+// for (key of iterable)
+
+// 7. Cloning
+// Object.assign(target, [obj1, obj2, obj3 ... ])
