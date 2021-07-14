43a98303d590de76a9ee6f0ea3d2cc49aa3b2e93
flowerYoungg <khy1896@gmail.com>
2021-07-14 21:31:14 +0900

    July14

---
+++ array.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// Array
+
+// 1. Declaration
+const arr1 = new Array();
+const arr2 = [1, 2];
+
+// 2. Index position
+
+// 3. Looping over an array
+// - for Loop
+// - for of Loop
+// - forEach Loop
+fruits.forEach((fruit, index) => console.log(fruit, index));
+
+// 4. Addtion, Deletion, Copy
+// push : add an item to the end
+// pop : remove an item from the end
+
+// unshift : add an item to the beginning
+// shift : remove an item from the beginning
+// - shift, unshift are slower than pop, push
+
+// splice : remove an item by index position
+// concat : combine two arrays
+
+// 5. Searching
+// indexOf : find the index
+// includes : return true / false
+// lastIndexOf : find the last index of the array
