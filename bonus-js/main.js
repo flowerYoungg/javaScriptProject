90749c638fffe94563463ab42a12cf9e63eb3828
flowerYoungg <khy1896@gmail.com>
2021-07-27 08:17:43 +0900

    July27

--- bonus-js/main.js
+++ bonus-js/main.js
@@ -15,3 +15,32 @@ function add(num1, num2) {
 }
 
 const sum = add(3, 4);
+
+//false : 0, -0, '', null, undefined
+//true : -1, 'hello', [], ...
+if (false) {
+  console.log("true!");
+} else {
+  console.log("false!");
+}
+
+class Counter {
+  constructor(runEveryFiveTimes) {
+    this.counter = 0;
+    this.callback = runEveryFiveTimes;
+  }
+
+  increase() {
+    this.counter++;
+    if (this.counter % 5 === 0) {
+      this.callback && this.callback(this.counter);
+    }
+  }
+}
+
+function printSomething(number) {
+  console.log(`Yeah! ${number}`);
+}
+
+const collCounter = new Counter(printSomething);
+collCounter.increase();
